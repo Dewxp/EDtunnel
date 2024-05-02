141dac90874994e77805caee49c954ca62c551eb
Dewxp <168750385+Dewxp@users.noreply.github.com>
2024-05-02 23:33:53 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'c3ff1402-7be7-457e-a01a-ed220307b9a5';
 
-const proxyIPs = ['35.219.7.133'];
+const proxyIPs = ['35.219.50.99'];
 
 // if you want to use ipv6 or single proxyIP, please add comment at this line and remove comment at the next line
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
